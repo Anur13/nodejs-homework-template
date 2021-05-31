13e49a4e3db121b23c7796a1d31ab2342d88ca40
Anur13 <akrupskyi13@gmail.com>
2021-05-31 12:53:05 +0300

    f

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,37 +4,18 @@ const jwt = require('jsonwebtoken')
 const User = require('../../service/schemas/users')
 const secret = process.env.SECRET
 const { auth } = require('../../service')
-const bCrypt = require('bcryptjs')
-
-const Joi = require('joi')
-
-const schema = Joi.object({
-  password: Joi.string()
-    .pattern(/^[a-zA-Z0-9]{3,30}$/)
-    .min(3)
-    .max(8)
-    .required(),
-  email: Joi.string().email().required()
-})
 
 router.post('/users/login', async (req, res, next) => {
-  const { value, error } = schema.validate(req.body)
-  const { email, password } = value
-  if (error) {
-    res.status(400).json({ message: error.message })
-    return
-  }
-
+  const { email, password } = req.body
   try {
     const user = await User.findOne({ email })
     const passwordCheck = await user.validPassword(password)
     if (!user || !passwordCheck) {
       return res.status(401).json({
         code: 401,
-        message: 'Email or password is wrong'
+        message: 'Incorrect login or password'
       })
     }
-    const hashedPass = bCrypt.hashSync(password, bCrypt.genSaltSync(6))
     const payload = {
       id: user.id,
       username: user.username
@@ -49,25 +30,21 @@ router.post('/users/login', async (req, res, next) => {
       }
     })
   } catch (error) {
-    res.status(400).json({ message: error })
+    console.log(error)
   }
 })
 
 router.post('/users/signup', async (req, res, next) => {
-  const { value, error } = schema.validate(req.body)
-  const { email, password, subscription } = value
+  const { email, password, subscription } = req.body
   const user = await User.findOne({ email })
-  if (error) {
-    return res.status(400).json({ message: error.message })
-  }
+
   if (user) {
     return res.status(401).json({
       message: 'Email in use'
     })
   }
   try {
-    const hashedPass = bCrypt.hashSync(password, bCrypt.genSaltSync(6))
-    const newUser = new User({ hashedPass, email, subscription })
+    const newUser = new User({ password, email, subscription })
     newUser.setPassword(password)
     await newUser.save()
     res.status(201).json({
@@ -79,26 +56,27 @@ router.post('/users/signup', async (req, res, next) => {
       }
     })
   } catch (error) {
+    console.log(error)
     res.status(400).json({ message: error.message })
   }
 })
 
 router.post('/users/logout', auth, async (req, res, next) => {
-  const id = req.user._id
+  const { email, password, subscription } = req.body
+  const user = await User.findOne({ email })
+  if (user) {
+    return res.status(409).json({
+      code: 409,
+      message: 'Email in use'
+    })
+  }
   try {
-    await User.findByIdAndUpdate({ _id: id }, { token: null }, { new: true })
-    res.status(204).json({
-      message: 'success'
+    res.status(201).json({
+      status: 'success',
+      code: 201
     })
   } catch (error) {
     res.status(400).json({ message: error.message })
   }
-})
-
-router.post('/users/current', auth, async (req, res, next) => {
-  const { email, subscription } = req.user
-  res.status(200).json({
-    message: { email, subscription }
-  })
 })
 module.exports = router
